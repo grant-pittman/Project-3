3bf0aacb58df6a2c6a56230537ac907f6a00e6dd
Grant Pittman <grantpittman@Grants-MBP.attlocal.net>
2020-09-08 22:11:46 -0500

    updating files for project

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-var trace1 = {
-    x: [{{country.year}}],
-    y: [{{country.score}}],
-    type: "scatter"
-};
+// var trace1 = {
+//     x: [country.year],
+//     y: [country.score],
+//     type: "scatter"
+// };
 
-var data = [trace1];
+// var data = [trace1];
 
-Plotly.newPlot("plot", data);
+// Plotly.newPlot("plot", data);
+
+
+// d3.json("/test", function(data) {
+//     console.log(data);
+// }); 
+
+var url = "/"
+
+d3.json(url,function(data){
+    console.log(data);
+});
--- app.py
+++ app.py
@@ -34,12 +34,31 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    """List all available api routes."""
-    return (
-        f"Available Routes:<br/>"
-        f"/api/v1.0/happiest_countries/year<br/>"
-        f"/api/v1.0/rank<br/>"
-    )
+    results = session.query(happiness.country, happiness.rank, happiness.score, happiness.economy, happiness.family, happiness.health, happiness.freedom, happiness.generosity, happiness.trust, happiness.year, happiness.lat, happiness.long).all()
+    data_dict = {}
+    data_list = []
+    for  country, rank, score, economy, family, health, freedom, generosity, trust, year, lat, long in results:
+        data_dict['Country'] = country
+        data_dict['Rank'] = rank
+        data_dict['Score']= score
+        data_dict['Economy']= economy
+        data_dict['Family']= family
+        data_dict['Health']= health
+        data_dict['Freedom']= freedom
+        data_dict['Generosity']= generosity
+        data_dict['Trust']= trust
+        data_dict['Year']= year
+        data_dict['Lat']= lat
+        data_dict['Long']= long
+        data_list.append(data_dict)
+        data_dict = {}
+        
+    return jsonify(data_list)
+
+@app.route("/website")
+def website():
+    return render_template("index.html")
+    
 
 
 @app.route("/api/v1.0/happiest_countries/<year>")
@@ -70,7 +89,8 @@ def one_country(country):
    capitalized = country.capitalize()
    results = session.query(happiness.rank, happiness.country, happiness.score, happiness.year).filter(happiness.country == capitalized).all()
    #country_list = list(results) 
-   return render_template("index.html", country=results)
+   return jsonify(results)
+   #render_template("index.html", country=results)
    
 if __name__ == '__main__':
     app.run(debug=True)
